53a517a66157ed8b66bbb8a1957869e913f978a9
Steven Moore <stevenallenmoore@gmail.com>
2019-09-16 08:46:09 -0400

    Updated for Fall 19

--- README.md
+++ README.md
@@ -1,226 +1,27 @@
-#### CS 240 - Spring 2019
-### PROGRAM 1 README FILE
+#### CS 240 - Fall 2019
+# Program 1 - Creating Larger Programs
+
+### [Assignment Description](https://docs.google.com/document/d/1ctTFW2OGH99OrfTZJBox6AdK7FnBuzebQPOFzRmGvCI/edit?usp=sharing)
+
+***
+
+_Replace anything surrounded by the `< >` symbols._
+
+## SUMMARY:
+    Team Members: <team member names>
+
+    < Provide a short description of your program's functionality. No more than a couple sentences. >
+
+## GRACE DAYS
+Grace days used for this assignment: < # >
+
+Grace days remaining: < # >/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
-- What parts of the program you were not able to complete
+ < What parts of the project you were not able to complete >
 
 ## REFERENCES:
-- List any outside resources used
+ < List any outside resources used >
 
 ## MISCELLANEOUS COMMENTS:
-- Anything you would like the grader to know
-
-***
-### Assignment Description
-***
-# Program 1 - Building Solar Systems
-## Due Date: 11:59 p.m., February 27th, 2019
-
-*All programs will be tested on the machines in the Q22 lab. If your code does not run on the system in this lab, it is considered non-functioning EVEN IF IT RUNS ON YOUR PERSONAL COMPUTER. Always check that your code runs on the lab machines before submitting.*
-
-### Driver Code and Test Files
-
-* program1.cpp
-    * include the driver code with your submission, but do not alter it
-
-### Grading Rubric
-
-**_TOTAL: 45 points_**
-
-* **Part A** (10 points): Vectors
-    * [2 pts] Test 1 Passed: creates a vector with properly initialized values
-    * [2 pts] Test 2-3 Passed: inserting into the vector passes all tests
-    * [2 pts] Test 4-6 Passed: reading from vector passes all tests
-    * [2 pts] Test 7 Passed: removing from vector passes tests
-    * [2 pts] Test 8-9 Passed:  deleting vector frees memory
-* **Part B** (10 points): Lists
-    * [2 pts] Test 10 Passed: creates a list with properly initialized values
-    * [2 pts] Test 11 Passed: inserting into list at index passes test
-    * [2 pts] Test 12 Passed: reading from list at index passes test
-    * [4 pts] Test 13 Passed: removing and deleting from list at index passes test
-* **Part C** (20 points): Stars
-    * [12 pts] Test 14 Passed: create, read, and remove from Starvector and Starlist without error
-    * [8 pts] Test 15 Passed: delete Starvector and Starlist without error
-* **Part D** (5 points): Profile
-   * [5 pts] completes profiling without error
-* **Style Guidelines and Memory Leaks**
-    * You will lose significant points for the following:
-        * Memory leak or error detected in valgrind  (-10 points)
-        * Repository includes .o files or binary [-1 point]
-        * Does not follow [Style guidelines](https://drive.google.com/open?id=1a5I7XhhCDRHoS8LUfILu3y2la4wW8HHh4olO30YvMVk)[-3 points]
-        * Your submission will not be accepted if:
-            * Does not follow requested project structure and submission format
-            * Does not compile
-
-### Guidelines
-
-This is a pair programming assignment. You and a partner can divide up the work. Although both of you may not work on all parts of the program you should understand and be able to fully explain every portion of the code. Outside of your team, it is permissible to consult with classmates to ask general questions about the assignment, to help discover and fix specific bugs, and to talk about high level approaches in general terms. It is not permissible to give or receive answers or solution details from fellow students.
-
-You may research online for additional resources; however, you may not use code that was written specifically to solve the problem you have been given, and you may not have anyone else help you or your partner write the code or solve the problem. You may use code snippets found online, providing that they are appropriately and clearly cited, within your submitted code.
-
-If you or your partner are found to have plagiarized any part of the assignment, both will receive a 0 and be reported.
-
-__In this program, you will learn to__:
-
-* Work with sequential and secondary data structures
-
-For the first program you and partner are going to build both a dynamic array (vector) and a linked list. Each data structure will be tested to ensure the validity of its operations. Once that is complete and you know your data structures are working, we are going to use them as internal data structures for our Star class. Both versions of the Star class will be evaluated to see which provides better performance.
-
-You will reuse your Planet class from lab 3 with no changes.
-
-:warning: *For all Data Structures, you may (and probably should) add additional functions, methods, and attributes, but what follows is the required minimum interface*
-
-:no_entry_sign: You may __not__ use the STL library for this program.
-
-***
-You will need to make a small change to your planet class constructor.
-
-#### Planet
-* `Planet(int distance)`
-    * You do not need to pass an ID. The ID will now be the address of the object.
-* `long int getID()`
-    * The return type of the getID() method will need to be changed to a long to hold the address (8 bytes)
-
-:warning: You will need to update your `Star` class method `addPlanet` accordingly.
-***    
-
-## Part A: Vectors
-You must break up your code into Vector.h and Vector.cpp according to the conventions we have discussed in class.
-
-Create a dynamic array data class, Vector. You must create your internal array on the heap (using `new`). Your Vector class should have, at minimum, the following:
-* `~Vector()`
-    * A destructor to clean up memory
-* `void insert(int index, Planet * p)`
-    * inserts an element at index, increasing the Vector size by 1
-    * if the insert index is out of bounds, you should increase the capacity to the size of the index + 1
-* `Planet* read(int index)`
-    * returns a pointer to the Planet object at `index`
-    * if the index is out of bounds, return NULL
-* `bool remove(int index)`
-    * remove the Planet object at `index`, decreasing the size of the Vector by 1.
-* `unsigned size()`
-    * returns the current size of the Vector (this may not be the same as the number of elements)
-
-## Part B: Linked Lists
-You must break up your code into List.h and List.cpp according to the conventions we discussed in class. Create a doubly linked list using a List and Node classes. :bulb: Your List and Node classes can go in the same file. Your linked list should have the following operations:
-* `List()`
-    * A pointer to a head and tail node, both initialized to NULL
-* `~List()`
-    * A destructor to clean up memory
-* `void insert(int index, Planet * p)`
-    * inserts an element at index, increasing the List size by 1
-    * if the insert index is out of bounds, you should append to the end of the list
-* `Planet* read(int index)`
-    * returns a pointer to the Planet object at `index`
-    * if the index is out of bounds, return NULL
-* `bool remove(int index)`
-    * remove the Planet object at `index`, decreasing the size of the Vector by 1.
-    * return `true` on successful deletion or `false` if the index is out of bounds
-* `unsigned size()`
-    * returns the current size of the List
-
-## Part C: Full of Stars
-
-Although you should have most of the code written from previous labs. You will need to make slight alterations to your Star class to work with the changes to the Planet class and to work with one of the Data Structures (Vector, List) you have created. You must break up your code into Star.h and Star.cpp according to the conventions we discussed in class.
-
-You will have two Star classes, Starlist and Starvector. You will be using your List and Vector, respectively, as the internal data structure to hold Planets.
-
-#### Starlist
-
-Your Starlist must have the following:
-
-* `Starlist()`
-    * Initialize memory
-* `~Starlist()`
-    * deallocate all memory when the Star is deleted.
-* `long addPlanet()`
-    * return the ID of the newly created Planet
-* `bool removePlanet(long)`
-    * Takes a Planet’s ID as a parameter, and removes the Planet from the Star.
-    * You must return `true` upon successful deletion and `false` on failure if the ID isn't found.
-* `Planet * getPlanet(long)`
-    * Takes a Planet’s ID and returns a pointer to the Planet. If the Planet is not found, it returns NULL.
-* `void orbit()`
-    * Iterate through your planets and alter their orbit position by +1
-* `void printStarInfo()`
-    * Prints the Star information.
-* `unsigned int getCurrentNumPlanets()`
-    * returns the current number of planets stored
-
-#### Starvector
-
-Your Starvector must have the following:
-* `Starvector()`
-    * Initialize memory
-* `~Starvector()`
-    * deallocate all memory when the Star is deleted.
-* `long addPlanet()`
-    * return the ID of the newly created Planet
-* `bool removePlanet(long)`
-    * Takes a Planet’s ID as a parameter, and removes the Planet from the Star.
-    * You must return `true` upon successful deletion and `false` on failure if the ID isn't found.
-* `Planet * getPlanet(long)`
-    * Takes a Planet’s ID and returns a pointer to the Planet. If the Planet is not found, it returns NULL.
-* `void orbit()`
-    * Iterate through your planets and alter their orbit position by +1
-* `void printStarInfo()`
-    * Prints the Star information.
-* `unsigned int getCurrentNumPlanets()`
-    * returns the current number of planets stored (the size of the vector)
-
-## Part D: Profiling
-
-Welcome to the easy part. If you made it this far, you can sit back and relax. I have written some profiling that test the operations for both versions of your Star class.
-
-Pay attention to the output.
-* What implementation is better for insertion?
-* Reading?
-* Overall?
-
-## Part E: Submission
-
-### Required file naming and organization:
-* program1.cpp //Driver Code
-* List.cpp/.h
-* Star.cpp/.h
-* Planet.cpp/.h
-* Vector.cpp/.h
-* makefile
-    * You must have the following labels in your makefile:
-        * all - to compile all your code to an executable called ‘program4’ (no extension). Do not run.
-        * run - to compile if necessary and run
-        * memcheck - to compile only if necessary and run with valgrind
-        * clean - to remove all executables and object files
-
-:no_entry: Every program will have a required submission guidelines. Please read submission requirements carefully. Any deviations from specifications will result in point deductions or incomplete grades.
-
-## README
-
-* KNOWN BUGS AND INCOMPLETE PARTS
-* REFERENCES
-* MISCELLANEOUS COMMENTS
-
-Before your final submission, edit the content for each of these sections in this README for your program. You do not have to use markdown, but you can find out more about markdown [here](https://guides.github.com/features/mastering-markdown/) if you would like to.
-
-### Git
-
-Below is a reminder of the commands you need to use to submit your program.
-
-:warning: You have created several files and even a folder for this program. Be sure you add them to the repo. If you forget to add the files to the repo, your assignment will be considered late or not accepted at all.
-
-```shell
-git status
-git add mylib.h
-git commit -a -m "commit message"
-git push
-```
-
-To find your most recent commit hash, use the following command:
-
-```shell
-git rev-parse HEAD
-```    
-
-To complete your submission, you must copy and paste this number into mycourses. Go to MyCourses, select cs240, then program1 under **Assignment Hash Submission**, and where it says text submission, paste your commit hash.
-
-:warning: You __MUST__ submit the commit hash on mycourses before the deadline to be considered on time **even if your program is completely working before the deadline**. :warning:
+ < Anything you would like the grader to know >
--- program1.cpp
+++ program1.cpp
@@ -13,9 +13,9 @@
 
 using namespace std;
 
-bool unique(long int ids[], int size){
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = i + 1; j < size; j++) {
+bool unique(unsigned long int ids[], unsigned int size){
+    for (unsigned int i = 0; i < size - 1; i++) {
+        for (unsigned int j = i + 1; j < size; j++) {
             if (ids[i] == ids[j]) {
                 return false;
             }
@@ -27,99 +27,134 @@ bool unique(long int ids[], int size){
 int main(){
 
     srand(time(NULL));
-    long id_list[200];
     cerr << "\t-----------------------------------\n";
     cerr << "\t-          Part A: Vectors        -\n";
     cerr << "\t-----------------------------------\n";
 
+#ifndef TEST1
+{
     cerr << "\n\t=========Test #1: Creating your Vector===========\n\n";
-
     Vector *vect = new Vector();
     assert(vect->size() == 0);
-    cerr << "\n\t=========Test #1 Passed...            ===========\n\n";
-
+    delete vect;
+    cerr << "\n\tTest #1 Passed...\n\n";
+}
+#endif
+#ifndef TEST2
+{
     cerr << "\n\t=========Test #2: Inserting 20 Planets into your Vector===========\n\n";
-
-    int i;
-    for(i = 0; i < 20 ; i++){
+    Vector *vect = new Vector();
+    for(int i = 0; i < 20 ; i++){
         vect->insert(i, new Planet(0));
     }
     assert(vect->size() == 20);
-
-    cerr << "\n\t=========Test #2 Passed...            ===========\n\n";
-
+    delete vect;
+    cerr << "\n\tTest #2 Passed...\n\n";
+}
+#endif
+#ifndef TEST3
+{
     cerr << "\n\t=========Test #3: Inserting values into out of bounds index===========\n\n";
-
+    Vector * vect = new Vector();
+    for(int i = 0; i < 20 ; i++){
+        vect->insert(i, new Planet(0));
+    }
     vect->insert(49, new Planet(0));
     assert(vect->size() == 50);
     vect->insert(40, new Planet(0));
     assert(vect->size() == 51);
-
-    cerr << "\n\t=========Test #3 Passed...            ===========\n\n";
-
+    delete vect;
+    cerr << "\n\tTest #3 Passed...\n\n";
+}
+#endif
+#ifndef TEST4
+{
     cerr << "\n\t=========Test #4: Reading from your Vector===========\n\n";
+    Vector *vect = new Vector();
+    for(int i = 0; i < 20 ; i++){
+        vect->insert(i, new Planet(0));
+    }
     Planet * p;
-    for(i = 0 ; i < 20 ; i++){
+    unsigned int id_list_size = 22;
+    unsigned long id_list[id_list_size] = {0};
+    for(unsigned int i = 0 ; i < id_list_size - 2 ; i++){
         p = vect->read(i);
         id_list[i] = p->getID();
     }
-    assert(unique(id_list, i));
+    assert(unique(id_list, id_list_size - 2));
+    vect->insert(49, new Planet(0));
+    vect->insert(40, new Planet(0));
     //the new index of planet 20 will be 50 because the previous insert would increment the index
     p = vect->read(50);
-    id_list[i++] = p->getID();
+    id_list[20] = p->getID();
     p = vect->read(40);
-    id_list[i++] = p->getID();
-    assert(unique(id_list, i));
-    cerr << "\n\t=========Test #4 Passed...            ===========\n\n";
-
-    cerr << "\n\t=========Test #5: Reading from unititialized vector index (should return NULL)===========\n\n";
-
-    assert(vect->read(45) == NULL);
-
-    cerr << "\n\t=========Test #5 Passed...            ===========\n\n";
-
+    id_list[21] = p->getID();
+    assert(unique(id_list, id_list_size));
+    delete vect;
+    cerr << "\n\tTest #4 Passed...\n\n";
+}
+#endif
+#ifndef TEST5
+{
+    cerr << "\n\t=========Test #5: Reading from uninitialized vector index (should return NULL)===========\n\n";
+    Vector *vect = new Vector();
+    vect->insert(5, new Planet(0));
+    for(int i = 0; i < 5; i++){
+        assert(vect->read(0) == NULL);
+    }
+    delete vect;
+    cerr << "\n\tTest #5 Passed...\n\n";
+}
+#endif
+#ifndef TEST6
+{
     cerr << "\n\t=========Test #6: Attempting to read from out of bounds===========\n\n";
-
+    Vector *vect = new Vector();
+    vect->insert(0, new Planet(0));
     assert(vect->read(100) == NULL);
-
-    cerr << "\n\t=========Test #6 Passed...            ===========\n\n";
-
+    delete vect;
+    cerr << "\n\tTest #6 Passed...\n\n";
+}
+#endif
+#ifndef TEST7
+{
     cerr << "\n\t=========Test #7: Remove elements from the vector ===========\n\n";
-
-    //RemoveVector
-    vect->remove(50);
-    vect->remove(40);
-    cerr << vect->size() << endl;
+    Vector *vect = new Vector();
+    for(int i = 0 ; i < 50; i++){vect->insert(i, new Planet(0));}
+    assert(!vect->remove(51));
+    assert(vect->remove(40));
     assert(vect->size() == 49);
-    cerr << "\n\t=========Test #7 Passed...            ===========\n\n";
-
+    delete vect;
+    cerr << "\n\tTest #7 Passed...\n\n";
+}
+#endif
+#ifndef TEST8
+{
     cerr << "\n\t=========Test #8: Remove elements from the vector randomly ===========\n\n";
-
+    Vector *vect = new Vector();
+    for(int i = 0 ; i < 50; i++){vect->insert(i, new Planet(0));}
     while(vect->size()){
         vect->remove((rand()%5));
     }
-    vect->remove(50);
-
-    cerr << "\n\t=========Test #8 Passed...            ===========\n\n";
-
-    cerr << "\n\t=========Test #9: Delete the vector from memory ===========\n\n";
-
     delete vect;
-    vect = NULL;
-
-    cerr << "\n\t=========Test #9 Passed...            ===========\n\n";
-
+    cerr << "\n\tTest #8 Passed...\n\n";
+}
+#endif
     cerr << "\t-----------------------------------\n";
     cerr << "\t-          Part B: Lists        -\n";
     cerr << "\t-----------------------------------\n";
-
-    cerr << "\n\t=========Test #10: Creating your Linked List===========\n\n";
+#ifndef TEST9
+{
+    cerr << "\n\t=========Test #9: Creating your Linked List===========\n\n";
     List * list = new List();
-
-    cerr << "\n\t\tTest #10: Passed ...\n\n";
-
-    cerr << "\n\t=========Test #11: Linked List Insertion===========\n\n";
-    int index = 0;
+    delete list;
+    cerr << "\n\tTest #9: Passed ...\n\n";
+}
+#endif
+#ifndef TEST10
+{
+    cerr << "\n\t=========Test #10: Linked List Insertion===========\n\n";
+    List * list = new List();
     //add elements to the list
     for(int index = 0 ; index < 10 ; index++ ){
         list->insert(index, new Planet(0));
@@ -128,22 +163,35 @@ int main(){
     //***** Test that out of bounds indexes are appended to the list ****/
     list->insert(20, new Planet(0));
     assert(list->size() == 11);
-
-    cerr << "\n\t\tTest #11: Passed ...\n\n";
-
-    cerr << "\n\t=========Test #12: Linked List Read===========\n\n";
-    for(i = 0 ; i < 11 ; i++){
-        p = list->read(i);
+    delete list;
+    cerr << "\n\tTest #10: Passed ...\n\n";
+}
+#endif
+#ifndef TEST11
+{
+    cerr << "\n\t=========Test #11: Linked List Read===========\n\n";
+    List * list = new List();
+    unsigned int id_list_size = 10;
+    unsigned long id_list[id_list_size];
+    for(unsigned int i = 0 ; i < id_list_size ; i++ ){list->insert(i, new Planet(0));}
+    for(unsigned int i = 0 ; i < id_list_size ; i++){
+        Planet * p = list->read(i);
         id_list[i] = p->getID();
     }
-    unique(id_list, 11);
-    p = list->read(20);
+    unique(id_list, id_list_size);
+    Planet * p = list->read(20);
     assert(p == NULL);
-    cerr << "\n\t\tTest #12: Passed ...\n\n";
-
-    cerr << "\n\t=========Test #13: Linked List Remove===========\n\n";
-
-    for(index = 0; list->size() > 4; index++){
+    delete list;
+    cerr << "\n\tTest #11: Passed ...\n\n";
+}
+#endif
+#ifndef TEST12
+{
+    cerr << "\n\t=========Test #12: Linked List Remove===========\n\n";
+    List * list = new List();
+    unsigned int list_size = 40;
+    for(unsigned int i = 0 ; i < list_size ; i++ ){list->insert(i, new Planet(0));}
+    for(unsigned int index = 0; list->size() > 4; index++){
         index = index % 4;
         unsigned int s = list->size();
         list->remove(index);
@@ -151,16 +199,20 @@ int main(){
     }
     assert(list->size() == 4);
     delete list;
-    list = NULL;
-    cerr << "\n\t\tTest #13 Passed...\n\n";
-
-    cerr << "\n\t=========Test #14: Star Classes ===========\n\n";
+    cerr << "\n\tTest #12 Passed...\n\n";
+}
+#endif
+#ifndef TEST13
+{
+    cerr << "\n\t=========Test #13: Star Classes Create and Read ===========\n\n";
     Starlist * sl = new Starlist();
+    unsigned int id_list_size = 2;
+    unsigned long id_list[id_list_size];
     assert(sl->getCurrentNumPlanets() == 0);
     id_list[0] = sl->addPlanet();
     assert(sl->getCurrentNumPlanets() == 1);
-    p = sl->getPlanet(id_list[0]);
-    int pos = p->getPos();
+    Planet * p = sl->getPlanet(id_list[0]);
+    unsigned int pos = p->getPos();
     sl->orbit();
     assert(p->getPos() == (pos + 1) % 360);
 
@@ -172,46 +224,56 @@ int main(){
     pos = p->getPos();
     sv->orbit();
     assert(p->getPos() == (pos + 1) % 360);
-
-    //add 200 more planets to each
-    for(int i = 0; i < 200; i+=2){
+    delete sl;
+    delete sv;
+    cerr << "\n\tTest #13 Passed...\n\n";
+}
+#endif
+#ifndef TEST14
+{
+    cerr << "\n\t=========Test #14: Star Classes Remove ===========\n\n";
+    Starlist * sl = new Starlist();
+    Starvector * sv = new Starvector();
+    unsigned int id_list_size = 200;
+    unsigned long id_list[id_list_size];
+    //add 100 planets to each
+    for(unsigned int i = 0; i < id_list_size; i+=2){
         id_list[i] = sv->addPlanet();
         id_list[i+1] = sl->addPlanet();
     }
-    //delete 10 random planets in the sun
-    for(int i = 0; i < 50; i++){
-        int rand_id = (rand() % (200));
-        if(id_list[rand_id] == -1){
+    //delete random planets in the star classes
+    for(unsigned int i = 0; i < id_list_size; i++){
+        unsigned int rand_id = (rand() % (id_list_size));
+        if(id_list[rand_id] == (unsigned) -1){
             continue;
-        }else if(sv->removePlanet(id_list[rand_id]) ||
-            sl->removePlanet(id_list[rand_id])){
-            id_list[rand_id] = -1;
+        }else if(sv->removePlanet(id_list[rand_id]) || sl->removePlanet(id_list[rand_id])){
+            id_list[rand_id] = (unsigned) -1;
         }else{
             assert(false);
         }
     }
-
     sv->printStarInfo();
     sl->printStarInfo();
-
-    cerr << "\n\t\tTest #14 Passed...\n\n";
-
-    cerr << "\n\t=========Test #15: Star Delete ===========\n\n";
     delete sl;
     delete sv;
-    cerr << "\n\t\tTest #15 Passed (but check valgrind to be sure)...\n\n";
-
+    cerr << "\n\tTest #14 Passed (but check valgrind to be sure)...\n\n";
+}
+#endif
+#ifndef TEST15
+{
+    cerr << "\n\t========='Test' #15: Relax ===========\n\n";
     cerr << "Comparing performance of Vectors and Lists\n";
-    sl = new Starlist();
-    sv = new Starvector();
-
+    Starlist * sl = new Starlist();
+    Starvector * sv = new Starvector();
+    unsigned int id_list_size = 200;
+    unsigned long id_list[id_list_size];
     struct timeval start, stop;
     float profile_time;
 
     /************************ Starvector Performance Test *********************/
     /** Testing performance of insert **/
     gettimeofday(&start, NULL);
-    for(int i = 0; i < 200; i++){
+    for(unsigned int i = 0; i < id_list_size; i++){
         id_list[i] = sv->addPlanet();
     }
     gettimeofday(&stop, NULL);
@@ -221,7 +283,7 @@ int main(){
 
     /** Testing performance of read **/
     gettimeofday(&start, NULL);
-    for(int i = 0; i < 200; i++){
+    for(unsigned int i = 0; i < id_list_size; i++){
         sv->getPlanet(id_list[i]);
     }
     gettimeofday(&stop, NULL);
@@ -230,7 +292,7 @@ int main(){
     printf("\tCompleted Starvector read profile time = %lf\n", profile_time);
     /** Testing performance of remove **/
     gettimeofday(&start, NULL);
-    for(int i = 0; i < 200; i+=5){
+    for(unsigned int i = 0; i < id_list_size; i+=rand()%5){
         sv->removePlanet(id_list[i]);
     }
     delete sv;
@@ -242,7 +304,7 @@ int main(){
     /************************ Starlist Performance Test *********************/
     /** Testing performance of insert **/
     gettimeofday(&start, NULL);
-    for(int i = 0; i < 200; i++){
+    for(unsigned int i = 0; i < id_list_size; i++){
         id_list[i] = sl->addPlanet();
     }
     gettimeofday(&stop, NULL);
@@ -252,7 +314,7 @@ int main(){
 
     /** Testing performance of read **/
     gettimeofday(&start, NULL);
-    for(int i = 0; i < 200; i++){
+    for(unsigned int i = 0; i < id_list_size; i++){
         sl->getPlanet(id_list[i]);
     }
     gettimeofday(&stop, NULL);
@@ -261,7 +323,7 @@ int main(){
     printf("\tCompleted Starlist read profile time = %lf\n", profile_time);
     /** Testing performance of remove **/
     gettimeofday(&start, NULL);
-    for(int i = 0; i < 200; i+=5){
+    for(unsigned int i = 0; i < id_list_size; i+=5){
         sl->removePlanet(id_list[i]);
     }
     delete sl;
@@ -270,6 +332,8 @@ int main(){
     profile_time = ((stop.tv_sec* 1000000) + stop.tv_usec) - ((start.tv_sec* 1000000) + start.tv_usec);
     printf("\tCompleted Starlist remove profile time = %lf\n", profile_time);
 
-    cerr << "\n\t========= Don't forget to submit your hash and check your code with Valgrind! ===========\n\n";
+    cerr << "\n\t========= All tests complete. Don't forget to submit your hash and check your code with Valgrind! ===========\n\n";
+}
+#endif
     return 0;
 }
